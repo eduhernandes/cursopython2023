652b38c17198fac892f6c6c0a8d0b64614d24bc0
eduhernandes <eduardo.hernandes@hotmail.com>
2024-02-01 23:25:51 -0300

    subir

--- teste.py
+++ teste.py
@@ -1,3 +1,10 @@
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
+
+
 """
 Introdução ao try/except
 try -> tentar executar o código
@@ -11,11 +18,6 @@ try:
     numero_float = float(numero_str)
     print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
-except Exception:
+except:
     print('Isso não é um número')
 
-# if numero_str.isdigit():
-#     numero_float = float(numero_str)
-#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
-# else:
-#     print('Isso não é um número')
